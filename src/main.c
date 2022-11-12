f57bcf8716449b09ffe521b87f335131d26e42c7
Peter Kubiak <peter.s.kubiak@bath.edu>
2022-11-12 23:28:44 +0000

    Add initialisation of the node ID

--- src/main.c
+++ src/main.c
@@ -1,4 +1,14 @@
+#include "rng_hal.h"
+
+#include "types.h"
+
 void main( void )
 {
+    uint8_t node_id;
+    if(rng_generate(&node_id, sizeof(node_id)) != RNG_OK)
+    {
+
+    }
+
     while( 1 ) {}
 }
