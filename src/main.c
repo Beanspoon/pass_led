b12e48dbebadb6243a1db808bf6ff730a7656890
Peter Kubiak <peter.s.kubiak@bath.edu>
2022-11-17 07:56:57 +0000

    Set LEDs as outputs in panic function

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include "rng_hal.h"
 
+#include "board_cfg.h"
+
 #include "types.h"
 
 void main( void )
@@ -15,6 +17,8 @@ void main( void )
 
 void panic( const char filename, const uint16_t line_number)
 {
+    gpio_configureOutputPin(LED0, GPIO_DRIVE_S0S1);
+    gpio_configureOutputPin(LED1, GPIO_DRIVE_S0S1);
     while(1)
     {
         
