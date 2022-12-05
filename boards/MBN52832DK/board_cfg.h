869d4f773a96aa20423b782a735788eedd360e70
Peter Kubiak <peter.s.kubiak@bath.edu>
2022-12-05 07:27:35 +0000

    Add GPIO initialisation

--- boards/MBN52832DK/board_cfg.h
+++ boards/MBN52832DK/board_cfg.h
@@ -3,6 +3,9 @@
 
 #include "gpio_hal.h"
 
+#define BTN0    GPIO_PIN13
+#define BTN1    GPIO_PIN14
+
 #define LED0    GPIO_PIN17
 #define LED1    GPIO_PIN18
 
--- src/main.c
+++ src/main.c
@@ -1,15 +1,27 @@
 #include "rng_hal.h"
+#include "gpio_hal.h"
 
 #include "panic.h"
+
+#include "board_cfg.h"
 
 #include <stdint-gcc.h>
 
 void main( void )
 {
+    // Generate ID
     uint32_t nodeId;
     if(rng_generate(&nodeId, sizeof(nodeId)) != RNG_OK)
     {
         panic();
     }
+
+    // Configure GPIO
+    gpio_configureInputPin(BTN0, GPIO_PULL_UP, GPIO_SENSE_DISABLED);
+    gpio_configureInputPin(BTN1, GPIO_PULL_UP, GPIO_SENSE_DISABLED);
+
+    gpio_configureOutputPin(LED0, GPIO_DRIVE_S0S1);
+    gpio_configureOutputPin(LED1, GPIO_DRIVE_S0S1);
+
     while( 1 ) {}
 }
