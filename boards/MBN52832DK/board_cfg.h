e0a4cfb1faefaa6620993eb29ae2ed2540059695
Peter Kubiak <peter.s.kubiak@bath.edu>
2022-12-18 21:36:27 +0000

    Add radio config

--- boards/MBN52832DK/board_cfg.h
+++ boards/MBN52832DK/board_cfg.h
@@ -2,14 +2,50 @@
 #define BOARD_CFG_H
 
 #include "gpio_hal.h"
+#include "radio_hal.h"
 
+#include "utils.h"
+
+// GPIO
 #define BTN0    GPIO_PIN13
 #define BTN1    GPIO_PIN14
 
 #define LED0    GPIO_PIN17
 #define LED1    GPIO_PIN18
 
+// CORE
 #define SYSTICK_TICK_FREQUENCY_HZ  1000u
 #define CORE_CLOCK_FREQUENCY_HZ    64000000u
+
+// RADIO
+#define RADIO_BASE_FREQUENCY                RADIO_BASE_FREQUENCY_2360MHZ
+#define RADIO_CHANNEL_FREQUENCY             0u
+
+#define DATA_WHITENING_IV                   13u
+
+#define RADIO_PRIMARY_ADDRESS_PREFIX        0xde
+#define RADIO_PRIMARY_ADDRESS_BASE          0xadbeef00
+#define RADIO_BASE_ADDRESS_LENGTH           RADIO_3_BYTE_BASE_ADDRESS
+#define RADIO_RX_ADDRESS                    RADIO_LOGADDR_PRIMARY
+
+#define RADIO_PACKET_PREAMBLE_LENGTH        RADIO_8_BIT_PREAMBLE
+#define RADIO_PACKET_S0_LENGTH              0u
+#define RADIO_PACKET_LENGTH_FIELD_LENGTH    4u
+#define RADIO_PACKET_S1_LENGTH              0u
+#define RADIO_S1_INCLUSION                  RADIO_AUTO_INCLUDE_S1
+#define RADIO_PACKET_MAX_PAYLOAD_LENGTH     8u
+#define RADIO_PACKET_STATIC_LENGTH          0u
+#define RADIO_PACKET_ENDIANNESS             RADIO_LITTLE_ENDIAN
+
+#define RADIO_CRC_LENGTH                    RADIO_3_BYTE_CRC
+#define RADIO_CRC_ADDRESS_BEHAVIOUR         RADIO_CRC_SKIP_ADDRESS
+#define RADIO_CRC_IV                        0x555555
+#define RADIO_CRC_POLYNOMIAL                BITMASK(10u) |\
+                                            BITMASK(9u) |\
+                                            BITMASK(6u) |\
+                                            BITMASK(4u) |\
+                                            BITMASK(3u) |\
+                                            BITMASK(1u) |\
+                                            BITMASK(0u)
 
 #endif
--- chipsets/nrf52832/include/radio_hal.h
+++ chipsets/nrf52832/include/radio_hal.h
@@ -13,6 +13,53 @@ typedef enum
     RADIO_BASE_FREQUENCY_2360MHZ
 } tRadio_baseFrequency;
 
+/**
+ * @brief Logical address enum
+ *
+ */
+typedef enum
+{
+    RADIO_LOGADDR_PRIMARY,
+    RADIO_LOGADDR1,
+    RADIO_LOGADDR2,
+    RADIO_LOGADDR3,
+    RADIO_LOGADDR4,
+    RADIO_LOGADDR5,
+    RADIO_LOGADDR6,
+    RADIO_LOGADDR7
+} tRadio_logicalAddress;
+
+/**
+ * @brief Radio base address length enum
+ * 
+ */
+typedef enum
+{
+    RADIO_2_BYTE_BASE_ADDRESS = 2,
+    RADIO_3_BYTE_BASE_ADDRESS = 3,
+    RADIO_4_BYTE_BASE_ADDRESS = 4
+} tRadio_baseAddressLength;
+
+typedef enum
+{
+    RADIO_LITTLE_ENDIAN,
+    RADIO_BIG_ENDIAN
+} tRadio_endianness;
+
+typedef enum
+{
+    RADIO_CRC_DISABLED,
+    RADIO_1_BYTE_CRC,
+    RADIO_2_BYTE_CRC,
+    RADIO_3_BYTE_CRC
+} tRadio_crcLength;
+
+typedef enum
+{
+    RADIO_CRC_INCLUDE_ADDRESS,
+    RADIO_CRC_SKIP_ADDRESS
+} tRadio_crcAddressBehaviour;
+
 void radio_configureFrequency(const uint8_t channelFrequency, const tRadio_baseFrequency baseFrequency);
 
 #endif  // RADIO_HAL_H
