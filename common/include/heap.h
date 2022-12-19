bd090a994cee00e4d961aa327b968d6584bb2831
Peter Kubiak <peter.s.kubiak@bath.edu>
2022-12-19 14:11:48 +0000

    Set up heap memory allocator interface

--- common/include/heap.h
+++ common/include/heap.h
@@ -4,11 +4,24 @@
 #include <stddef.h>
 
 /**
+ * @brief Initialises the heap memory allocator
+ * 
+ */
+void heap_init(void);
+
+/**
  * @brief Allocates heap memory
  * 
  * @param[in] size The size of the memory to be allocated in bytes
  * @return void* pointer to the allocated memory
  */
 void* malloc(const size_t size);
+
+/**
+ * @brief Free allocated memory
+ * 
+ * @param[in] ptr Pointer to the memory to be freed
+ */
+void free(void *ptr);
 
 #endif // HEAP_H
---
+++ common/src/heap.c
@@ -0,0 +1,10 @@
+#include "heap.h"
+
+#include <stdint-gcc.h>
+
+extern uint32_t __estack, __ram_end;
+
+void heap_init(void)
+{
+
+}
