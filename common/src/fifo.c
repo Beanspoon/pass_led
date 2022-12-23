1a998017df6a9d1b89d434051d6a92d3740b90e2
Peter Kubiak <peter.s.kubiak@bath.edu>
2022-12-23 01:08:00 +0000

    Add getNextLocationForPointer static function to FIFO buffer

--- common/src/fifo.c
+++ common/src/fifo.c
@@ -4,9 +4,22 @@
 
 #include <stdint-gcc.h>
 
-static void UpdatePointer(const void * pointer, const tFifo_instance * const pInstance)
+/**
+ * @brief Find the next location for the pointer given that it will be advanced by one element
+ * This will take into account wrapping if the end of the buffer is reached
+ * 
+ * @param[in] pointer The pointer in question
+ * @param pInstance Pointer to the FIFO buffer instance with which the pointer is associated
+ * @return void* indicating the new address to which the pointer will be pointing
+ */
+static void* getNextLocationForPointer(const void* const pointer, const tFifo_instance* const pInstance)
 {
-    
+    const uint8_t * const pByteAligned = (uint8_t *)pointer;
+    const uint8_t *pNextLocation = pByteAligned + pInstance->elementSize;
+    const size_t bufferSize_bytes = pInstance->elementSize * pInstance->bufferSize;
+    const uint8_t * const pStart = (uint8_t *)pInstance->pBuffer;
+    pNextLocation = (pNextLocation < pStart + bufferSize_bytes) ? pNextLocation : pStart;
+    return (void *)pNextLocation;
 }
 
 tFifo_status fifo_write(tFifo_instance * const pInstance, const void * const pElement)
